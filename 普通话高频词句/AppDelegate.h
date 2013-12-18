5f6838029d3f28be75bfcd8115ec950f87f08c08
dee <deexie1990@gmail.com>
2013-12-18 13:17:26 +0800

    Initial Commit

---
+++ 普通话高频词句/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  普通话高频词句
+//
+//  Created by xieweizhi on 12/18/13.
+//  Copyright (c) 2013 xieweizhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 普通话高频词句/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  普通话高频词句
+//
+//  Created by xieweizhi on 12/18/13.
+//  Copyright (c) 2013 xieweizhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
